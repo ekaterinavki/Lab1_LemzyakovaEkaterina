a92b58617843e275a254d7caae9398f7e027f2f6
ekaterinavki <129531889+ekaterinavki@users.noreply.github.com>
2023-09-17 22:34:14 +0800

    Programm

--- Program.cs
+++ Program.cs
@@ -1,183 +1,134 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.IO;
 using System.Text.RegularExpressions;
-
+using System.IO;
+using System.ComponentModel;
 
 class Program
 {
-    static string LogFilePath = "log.txt";
     static void Main(string[] args)
     {
-        //входные данные
-        bool? success = null;
-
-        string login = GetValidLogin();//Пример: +1-123-456-7890
-        string password = GetValidPassword();//Пример: Password123!
-        string confirmPassword = GetConfirmPassword(password);
-
-        string result = success.HasValue ? success.Value.ToString() : "False";
-        string message = success.HasValue ? "" : "Вывод: " + GetRegistrationErrorMessage(password, confirmPassword);
-
-        Console.WriteLine("Строка1 (результат): " + result);
-        Console.WriteLine("Строка2 (сообщение): " + message);
+        RegisterUser();
 
         Console.ReadKey();
     }
 
-    //метод считывания логина с консоли и проверка его правильности
-    static string GetValidLogin()
+    static void RegisterUser()
     {
+        // Входные данные
         Console.WriteLine("Введите логин:");
         string login = Console.ReadLine();
 
-        if (IsValidPhoneNumber(login) || IsValidEmail(login) || IsValidString(login))
-        {
-            if (IsValidString(login) && login.Length >= 5 && IsUniqueLogin(login))
-            {
-                return login;
-            }
-            else
-            {
-                Console.WriteLine("Логин должен содержать минимум 5 символов и состоять только из латиницы, цифр и знака подчеркивания.");
-                return GetValidLogin();
-            }
-        }
-        else
-        {
-            Console.WriteLine("Неправильный формат логина. Логин должен быть в формате телефона (+x-xxx-xxx-xxxx), электронной почты (xxx@xxx.xxx) или просто строки символов.");
-            return GetValidLogin();
-        }
-    }
-
-    //проверка формата логина
-    static bool IsValidPhoneNumber(string phoneNumber)
-    {
-        string pattern = @"^\+\d{1,}-\d{3}-\d{3}-\d{4}$";
-        return Regex.IsMatch(phoneNumber, pattern);
-    }
-
-    static bool IsValidEmail(string email)
-    {
-        string pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
-        return Regex.IsMatch(email, pattern);
-    }
-
-    static bool IsValidString(string str)
-    {
-        string pattern = @"^[a-zA-Z0-9_]+$";
-        return Regex.IsMatch(str, pattern);
-    }
-
-    static bool IsUniqueLogin(string login)
-    {
-        // Проверка на уникальность логина
-        string[] predefinedLogins = { "admin", "user1", "user2" };
-        foreach(string predefinedLogin in predefinedLogins)
-        {
-            if(predefinedLogin.ToLower() == login.ToLower())
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    //метод считывания пароля с консоли и проверка его правильности
-    static string GetValidPassword()
-    {
         Console.WriteLine("Введите пароль:");
         string password = Console.ReadLine();
 
-        if (IsValidPassword(password))
-        {
-            return password;
-        }
-        else
-        {
-            Console.WriteLine("Пароль должен содержать минимум 7 символов, латиницу, цифры и спецсимволы, а также минимум одну букву в верхнем и нижнем регистре.");
-            return GetValidPassword();
-        }
-    }
-
-    //проверка формата пароля
-    static bool IsValidPassword(string password)
-    {
-        string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{7,}$"; ;
-        return Regex.IsMatch(password, pattern);
-    }
-
-    //метод который считывает подтверждение пароля с консоли и проверяет его совпадение с введенным паролем
-    static string GetConfirmPassword(string password)
-    {
         Console.WriteLine("Подтвердите пароль:");
         string confirmPassword = Console.ReadLine();
 
-        if (confirmPassword == password)
+        // Выходные данные
+        bool result = false;
+        string message = "";
+
+        // Проверка данных пользователя
+        if (IsLoginValid(login) && IsPasswordValid(password) && IsConfirmPasswordValid(password, confirmPassword))
         {
-            return confirmPassword;
+            result = true;
         }
         else
         {
-            Console.WriteLine("Подтверждение пароля не совпадает с введенным паролем. Пожалуйста, попробуйте снова.");
-            return GetConfirmPassword(password);
+            message = "Ошибка при регистрации";
+        }
+
+        // Запись результатов в лог
+        string logText = GetFormattedLogText(login, password, confirmPassword, result, message);
+        LogToFile(logText);
+        LogToConsole(logText);
+    }
+
+    static bool IsLoginValid(string login)
+    {
+        try
+        {
+            // Проверка формата логина
+            Regex phoneRegex = new Regex(@"^\+\d{1}-\d{3}-\d{3}-\d{4}$");
+            Regex emailRegex = new Regex(@"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$");
+
+            if (phoneRegex.IsMatch(login) || emailRegex.IsMatch(login) || login.Length >= 5)
+            {
+                // Проверка логина на уникальность
+                string[] predefinedLogins = { "admin", "user1", "user2" };
+                foreach (string predefinedLogin in predefinedLogins)
+                {
+                    if (predefinedLogin.ToLower() == login.ToLower())
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                throw new Exception("Неверный формат логина");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+            return false;
         }
     }
 
-    //метод который возвращает сообщение об ошибке при регистрации
-    static string GetRegistrationErrorMessage(string password, string confirmPassword)
+    static bool IsPasswordValid(string password)
     {
-        string errorMessage = "";
+        // Проверка формата пароля
+        Regex passwordRegex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{7,}$");
 
-        if (password.Length < 8)
-            errorMessage = "Пароль должен быть не менее 8 символов";
-        else if (password != confirmPassword)
-            errorMessage = "Подтверждение пароля не совпадает";
-        else if (password == confirmPassword)
-            errorMessage = "Регистрация прошла успешно";
+        return passwordRegex.IsMatch(password);
+    }
+
+    //проверка паролей на совпадение
+    static bool IsConfirmPasswordValid(string password, string confirmPassword)
+    {
+        return password == confirmPassword;
+    }
+
+    static string GetFormattedLogText(string login, string password, string confirmPassword, bool result, string message)
+    {
+        string logText = DateTime.Now.ToString() + " - ";
+        logText += "Login: " + MaskPassword(login) + ", ";
+        logText += "Password: " + MaskPassword(password) + ", ";
+        logText += "Confirm Password: " + MaskPassword(confirmPassword) + ", ";
+
+        if (result)
+        {
+            logText += "Результат: Успешная регистрация";
+        }
         else
-            errorMessage = "Неизвестная ошибка";
-        return errorMessage;
+        {
+            logText += "Результат: " + message;
+        }
+
+        return logText;
     }
 
-    //Методы записывающие информацию о регистрации в файл и выводят ее на консоль
-    static void LogSuccessfulRegistration(string login, string password, string confirmPassword)
-    {
-        string logMessage = string.Format("{0} - {1} - {2} - {3} - Успешная регистрация",
-        DateTime.Now.ToString(), login, MaskPassword(password), MaskPassword(confirmPassword));
-
-        LogToFile(logMessage);
-        Console.WriteLine(logMessage);
-    }
-
-    static void LogUnsuccessfulRegistration(string login, string password, string confirmPassword, string errorMessage)
-    {
-        string logMessage = string.Format("{0} - {1} - {2} - {3} - {4}",
-        DateTime.Now.ToString(), login, MaskPassword(password), MaskPassword(confirmPassword), errorMessage);
-
-        LogToFile(logMessage);
-        Console.WriteLine(logMessage);
-    }
-
+    //маскирование пароля
     static string MaskPassword(string password)
     {
-        // Маскирование пароля
         return new string('*', password.Length);
     }
 
     //записывает лог в файл "log.txt"
-    static void LogToFile(string message)
+    static void LogToFile(string logText)
     {
-        using (StreamWriter sw = File.AppendText(LogFilePath))
+        using (StreamWriter sw = File.AppendText("log.txt"))
         {
-            sw.WriteLine(message);
+            sw.WriteLine(logText);
         }
     }
 
     //выводит лог в консоль
-    static void LogToConsole(string message)
+    static void LogToConsole(string logText)
     {
-        Console.WriteLine(message);
+        Console.WriteLine(logText);
     }
 }
--- obj/Debug/net6.0/Labr1.AssemblyInfo.cs
+++ obj/Debug/net6.0/Labr1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lab1.1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Labr1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lab1.1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lab1.1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Labr1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Labr1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
